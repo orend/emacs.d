be918b377523c39d7522af0db863aa7e31666604
Oren Dobzinski <oren@desk.com>
2015-10-22 21:22:34 -0400

    use melpa stable

--- setup/setup-required-packages.el
+++ setup/setup-required-packages.el
@@ -1,7 +1,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
