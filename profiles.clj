b03af697a8e48ce63e2167d5d8746830597821f5
Oren Dobzinski <odobzinski@salesforce.com>
2018-08-23 13:39:48 -0400

    fix nrepl

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,4 @@
 {:user {:plugins [[lein-try "0.4.3"]
                   [lein-ancient "0.5.5"]
-                  [cider/cider-nrepl "0.12.0"]
                   [lein-exec "0.3.1"]
-                  [refactor-nrepl "2.2.0"]]}}
+                  ]}}
