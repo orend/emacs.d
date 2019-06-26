fa86bc840479fd717cad5ff5372a10a62bb0be88
Oren Dobzinski <oren@desk.com>
2019-06-26 23:56:48 +0300

    don't format on save

--- setup/setup-meghanada.el
+++ setup/setup-meghanada.el
@@ -8,7 +8,8 @@
             (flycheck-mode +1)
             (setq c-basic-offset 4)
             ;; use code format
-            (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)))
+            ;; (add-hook 'before-save-hook 'meghanada-code-beautify-before-save)
+            ))
 (cond
    ((eq system-type 'windows-nt)
     (setq meghanada-java-path (expand-file-name "bin/java.exe" (getenv "JAVA_HOME")))
