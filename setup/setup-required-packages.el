d7a42dcc475d78b0a3af39d34ce43aac643b0958
Oren Dobzinski <odobzinski@salesforce.com>
2020-09-21 16:01:17 -0400

    hack to solve kotlin-mode-not-found error

--- setup/setup-required-packages.el
+++ setup/setup-required-packages.el
@@ -1,10 +1,22 @@
 (require 'package)
 
+; (add-to-list 'package-archives
+;             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
+
+(cond
+ ((>= 24 emacs-major-version)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives
+           '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+  (package-refresh-contents)
+ )
+)
 
 (package-initialize)
 
